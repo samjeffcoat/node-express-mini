82d3e0676bfa195f424b3d67cd0e6af6d68138fc
samjeffcoat <samjeffcoat@gmail.com>
2019-02-18 14:33:44 -0600

    Sets up server. Server is running and ready for changes

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // implement your API here
+const express = require('express');
+
+const db= require('./data/db');
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    res.send('hello you')
+})
+
+
+
+
+server.listen(4000, () => {
+    console.log('\n*** Server Running on http://localhost:4000 ***\n')
+})
